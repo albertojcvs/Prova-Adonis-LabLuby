5ec7936e21d4d78d2c4147c3f52758390c8b399e
Alberto Jose <albertojcvs@gmail.com>
2021-11-29 10:11:15 -0300

    feat: add the User model code

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,9 +1,33 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import Bet from './Bet'
+import Permission from './Permission'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public username:string
+
+  @column()
+  public email:string
+
+  @column()
+  public password:string
+
+  @hasMany(( () => Bet), {
+    foreignKey:'user_id'
+  })
+  public bets:HasMany<typeof Bet>
+
+  @manyToMany(() => Permission,{
+    pivotForeignKey:'user_id',
+    relatedKey:'id',
+    pivotRelatedForeignKey:'permission_id',
+    pivotTable: 'user_permissions'
+  })
+  public permissions: ManyToMany<typeof Permission>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
