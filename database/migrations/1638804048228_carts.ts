730aab861028243913e8c7492505c81341ca9196
Alberto Jose <albertojcvs@gmail.com>
2021-12-06 14:07:30 -0300

    add the cart model and migration

--- database/migrations/1638804048228_carts.ts
+++ database/migrations/1638804048228_carts.ts
@@ -7,11 +7,10 @@ export default class Carts extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.float('min_value').notNullable();
+
+      table.dateTime('created_at', { useTz: true })
+      table.dateTime('updated_at', { useTz: true })
     })
   }
 
---
+++ database/seeders/Cart.ts
@@ -0,0 +1,8 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Cart from 'App/Models/Cart'
+
+export default class CartSeeder extends BaseSeeder {
+  public async run () {
+   await Cart.create({minValue:30})
+  }
+}
