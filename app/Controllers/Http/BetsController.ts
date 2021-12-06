f3eef1886ee4a7228d1edeb202705cece768187f
Alberto Jose <albertojcvs@gmail.com>
2021-12-06 14:11:33 -0300

    fix the errors messages and fix how validate the price of cart

--- app/Controllers/Http/BetsController.ts
+++ app/Controllers/Http/BetsController.ts
@@ -5,6 +5,12 @@ import Bet from 'App/Models/Bet'
 import Game from 'App/Models/Game'
 import CreateBetValidator from 'App/Validators/CreateBetValidator'
 import User from 'App/Models/User'
+import Cart from 'App/Models/Cart'
+
+interface BetToMail {
+  numbers: number[]
+  game_name: string
+}
 
 export default class BetsController {
   public async index() {
@@ -14,23 +20,31 @@ export default class BetsController {
   public async store({ request, response }: HttpContextContract) {
     const { bets } = await request.validate(CreateBetValidator)
 
-    const betsToEmail: { numbers: number[]; game_name: string }[] = []
+    const cart = await Cart.firstOrFail()
+
+    const betsToEmail: BetToMail[] = []
+
+    let sumOfBets = 0
 
     for (let bet of bets) {
       const game = await Game.findOrFail(bet.game_id)
 
       betsToEmail.push({ numbers: bet.numbers, game_name: game.type })
 
+      sumOfBets += game.price
+
       if (bet.numbers.length > game.max_number) {
         return response
           .status(409)
-          .send({error:{message:'Some of the bets have more numbers than the  game max numbers!'}})
+          .send({
+            error: { message: 'Some of the bets have more numbers than the  game max numbers!' },
+          })
       }
       const betHasNumbersGreaterThanGameRange = bet.numbers.some((number) => number > game.range)
       if (betHasNumbersGreaterThanGameRange) {
         return response
           .status(409)
-          .send({error:{ message: 'Some of the bets have a numbers greather than game range!' }})
+          .send({ error: { message: 'Some of the bets have a numbers greather than game range!' } })
       }
 
       const betNumbersInString = bet.numbers.join(', ')
@@ -43,8 +57,14 @@ export default class BetsController {
         .first()
 
       if (betAlreadyExists) {
-        return response.status(409).send({error:{ message: 'Some of the bets already exits! ', bet }})
+        return response
+          .status(409)
+          .send({ error: { message: 'Some of the bets already exits! ', bet } })
       }
+    }
+
+    if (sumOfBets < cart.minValue) {
+      return response.status(409).send({error:{message: `The minimun cart value must be ${cart.minValue}`}})
     }
 
     const newBets = await Bet.createMany(
@@ -66,7 +86,7 @@ export default class BetsController {
         .subject('Your new bets!')
         .htmlView('emails/new_bets', { bets: betsToEmail, username: user.username })
     })
-    return {success:{ message: 'Bets have been created!', bets: newBets }}
+    return  newBets
   }
 
   public async show({ params }: HttpContextContract) {
@@ -83,6 +103,6 @@ export default class BetsController {
     await bouncer.authorize('deleteBet', bet)
 
     await bet.delete()
-    return {success:{message: 'User has been deleted!'}}
+    return { success: { message: 'User has been deleted!' } }
   }
 }
