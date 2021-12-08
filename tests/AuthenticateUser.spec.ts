ae5a7dae1691ba93f8f0b370d136ab8bf03fa712
Alberto Jose <albertojcvs@gmail.com>
2021-12-08 08:50:15 -0300

    add new test at create user

---
+++ tests/AuthenticateUser.spec.ts
@@ -0,0 +1,9 @@
+import User from 'App/Models/User'
+import test from 'japa'
+import supertest from 'supertest'
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+
+test.group('Authenticate user',() => {
+  test('', () => {})
+})
--- tests/CreateUser.spec.ts
+++ tests/CreateUser.spec.ts
@@ -1,7 +1,6 @@
 import User from 'App/Models/User'
 import test from 'japa'
 import supertest from 'supertest'
-import axios from 'axios'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Create a User', () => {
